325399d1c8ce1333930871f057c330042d0aa6f8
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-02-01 20:53:47 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,9 +1,7 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.2]
   def change 
     add_column :students, :grade, :integer 
-  end 
-  
-  def change
+
     add_column :students, :birthdate, :string 
   end 
 end 
